809f5cdea94a71e49cef75bbcc8af1e600047b69
Nicolas Silva <nic.silvaa@duocuc.cl>
2025-12-12 19:53:19 -0300

    Change components names and imports

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,18 +3,18 @@ import { Routes, Route } from "react-router";
 import Loading from "../components/loading/Loading";
 
 const HomePage = React.lazy( () => import("../pages/HomePage") )
-const EventsPage = React.lazy( () => import("../pages/EventsPage"))
-const EventDetailPage = React.lazy( () => import("../pages/EventDetailPage"))
+const RecipesPage = React.lazy( () => import("../pages/RecipesPage"))
+const RecipeDetailPage = React.lazy( () => import("../pages/RecipeDetailPage"))
 const AboutUsPage = React.lazy( () => import("../pages/AboutUsPage"))
 
 
-const AppRoutes = ({listaEventos, loading}) => {
+const AppRoutes = ({listaRecetas, loading}) => {
   return ( 
     <Suspense fallback={<Loading />} >
       <Routes>
         <Route path="/" element={<HomePage/>} />
-        <Route path="/events" element={<EventsPage listaEventos={listaEventos} loading={loading}/>} />
-        <Route path="/events/:id" element={<EventDetailPage listaEventos={listaEventos} />} />
+        <Route path="/recipes" element={<RecipesPage listaRecetas={listaRecetas} loading={loading}/>} />
+        <Route path="/recipes/:id" element={<RecipeDetailPage listaRecetas={listaRecetas} />} />
         <Route path="/about-us" element={<AboutUsPage />} />
       </Routes>
     </Suspense>
